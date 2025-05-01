03b6bc81160886adcf8dbaff5994e0710262feab
Reyes Lawson <reyeslawson89@gmail.com>
2025-05-01 17:23:02 -0500

    reactive touches

--- src/Components/GenreWeb/GenreWeb.jsx
+++ src/Components/GenreWeb/GenreWeb.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./GenreWeb.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Container } from 'react-bootstrap';
+import { Container } from "react-bootstrap";
 
 function App() {
   const [genres, setGenres] = useState([]);
@@ -24,7 +24,6 @@ function App() {
   }, []);
 
   const fetchAndStartCycling = () => {
-    // Fetch movies based on the selected genre
     fetch(
       `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=${selectedGenre}`
     )
@@ -47,7 +46,6 @@ function App() {
       .catch((error) => console.error("Error fetching movies:", error));
   };
 
-  // Select a random movie from the fetched movies
   const getRandomMovie = () => {
     if (movies.length > 0) {
       const randomIndex = Math.floor(Math.random() * movies.length);
@@ -61,11 +59,11 @@ function App() {
     <Container className="title">
       <h1 className="peralta-regular">Not Sure What To Watch ?</h1>
 
-      {/* Dropdown for genres */}
       <select
         className="dropDown"
         onChange={(e) => setSelectedGenre(e.target.value)}
-        value={selectedGenre}>
+        value={selectedGenre}
+      >
         <option value="">Select Genre</option>
         {genres.map((genre) => (
           <option key={genre.id} value={genre.id}>
@@ -74,17 +72,16 @@ function App() {
         ))}
       </select>
 
-      <div className="overall-container ">
-        <div className="icon-container">
+      <div className="overall-container">
+        <div className="col-lg-2 col-4 icon-container">
           <button className="button-container" onClick={fetchAndStartCycling}>
-            <i className="bi bi-camera-reels large-icon bi:hover"></i>
+            <i className="bi bi-camera-reels large-icon camera-button"></i>
           </button>
         </div>
 
         <section className="movie-container">
           {cycling && movies.length > 0 && (
-            <div className="">
-              {/* <h2>{movies[currentMovieIndex]?.title}</h2> */}
+            <div className="col-lg-9 col-12">
               <img
                 src={`https://image.tmdb.org/t/p/w200${movies[currentMovieIndex]?.poster_path}`}
                 alt={movies[currentMovieIndex]?.title}
@@ -92,20 +89,20 @@ function App() {
             </div>
           )}
 
-          {/* Display random movie */}
           {randomMovie && !cycling && (
-            <div className="about-movie-button">
-              {/* <h2>{randomMovie.title}</h2> */}
+            <div className="col-lg-9 col-12 image-container">
               <img
+                className="image-container"
                 src={`https://image.tmdb.org/t/p/w200${randomMovie.poster_path}`}
                 alt={randomMovie.title}
               />
               {!showInfo ? (
                 <button
                   className="bi bi-info-circle-fill d-flex justify-content-center mt-3 aboutButton"
-                  onClick={() => setShowInfo(true)}></button>
+                  onClick={() => setShowInfo(true)}
+                ></button>
               ) : (
-                <p className="movieInfo">{randomMovie.overview}</p>
+                <div className="movieInfo">{randomMovie.overview}</div>
               )}
             </div>
           )}
